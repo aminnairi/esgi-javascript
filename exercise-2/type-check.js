c655d9e50bc3ed4e4871cf5954861d18eb5ce241
Amin NAIRI <nairi.amin@gmail.com>
2019-11-29 11:40:03 +0100

    :package: production build

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,4 +1,5 @@
 "use strict";
+;
 function type_check_v1(input, expectedType) {
     if (arguments.length !== 2) {
         throw new Error("Expected exactly two arguments");
@@ -9,4 +10,16 @@ function type_check_v1(input, expectedType) {
     const inputType = Object.prototype.toString.call(input).toLowerCase();
     const computedExpectedType = `[object ${expectedType.toLowerCase()}]`;
     return inputType === computedExpectedType;
+}
+function type_check_v2(input, options) {
+    if (options.hasOwnProperty("type") && !type_check_v1(input, options.type)) {
+        return false;
+    }
+    if (options.hasOwnProperty("value") && input !== options.value) {
+        return false;
+    }
+    if (options.hasOwnProperty("enum") && !options.enum.includes(input)) {
+        return false;
+    }
+    return true;
 }
