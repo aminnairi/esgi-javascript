e7c08b401afddc76d5159665d9ea92a5bdcee33a
Amin NAIRI <nairi.amin@gmail.com>
2019-11-29 11:05:04 +0100

    :package: production build

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,5 +1,11 @@
 "use strict";
 function type_check_v1(input, expectedType) {
+    if (arguments.length !== 2) {
+        throw new Error("Expected exactly two arguments");
+    }
+    if (typeof expectedType !== "string") {
+        throw new TypeError("Expected second argument to be a string");
+    }
     const inputType = Object.prototype.toString.call(input).toLowerCase();
     const computedExpectedType = `[object ${expectedType.toLowerCase()}]`;
     return inputType === computedExpectedType;
