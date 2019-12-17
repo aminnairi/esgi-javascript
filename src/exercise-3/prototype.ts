58b404a57a2a78f1c36dbb650e1660e607660c3c
Amin NAIRI <nairi.amin@gmail.com>
2019-12-17 15:28:21 +0100

    :sparkles: added capitalize prototype to string

--- src/exercise-3/prototype.ts
+++ src/exercise-3/prototype.ts
@@ -1,11 +1,16 @@
-import { ucfirst } from "../exercise-1/string"
+import { ucfirst, capitalize } from "../exercise-1/string"
 
 declare global {
     interface String {
         ucfirst(): string;
+        capitalize(): string;
     }
 }
 
 String.prototype.ucfirst = function() {
     return ucfirst(this);
+};
+
+String.prototype.capitalize = function() {
+    return capitalize(this);
 };
