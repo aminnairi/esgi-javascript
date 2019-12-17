494180aacffa8ac54f2923bb543a15509d6a2896
Amin NAIRI <aminnairi@users.noreply.github.com>
2019-12-17 15:29:22 +0100

    :sparkles: added capitalize prototype (#23)

--- src/exercise-3/prototype.spec.ts
+++ src/exercise-3/prototype.spec.ts
@@ -6,7 +6,7 @@ import { expect } from "chai";
 describe("prototype", function() {
     describe("ucfirst", function() {
         it("should be defined in the String.prototype", function() {
-            expect(String.prototype.ucfirst).to.not.be.null;
+            expect(String.prototype.ucfirst).to.not.be.undefined;
         });
 
         it("should return the uppercase first letter", function() {
@@ -14,6 +14,19 @@ describe("prototype", function() {
             expect("JAVASCRIPT".ucfirst()).to.equal("JAVASCRIPT");
             expect("JavaScript".ucfirst()).to.equal("JavaScript");
             expect("javaScript".ucfirst()).to.equal("JavaScript");
+        });
+    });
+
+    describe("capitalize", function() {
+        it("should be defined in the String.prototype", function() {
+            expect(String.prototype.capitalize).to.not.be.undefined;
+        });
+
+        it("should return the uppercase words", function() {
+            expect("bonjour le monde".capitalize()).to.equal("Bonjour Le Monde");
+            expect("BONJOUR LE MONDE".capitalize()).to.equal("Bonjour Le Monde");
+            expect("Bonjour Le Monde".capitalize()).to.equal("Bonjour Le Monde");
+            expect("bonjour_le_monde".capitalize()).to.equal("Bonjour_le_monde");
         });
     });
 });
