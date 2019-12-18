4d75b68ef1965b565a06637ced802fb79e18f9c1
Amin NAIRI <nairi.amin@gmail.com>
2019-12-18 10:24:21 +0100

    :sparkles: added vig to string prototype

--- src/exercise-3/prototype.ts
+++ src/exercise-3/prototype.ts
@@ -1,4 +1,4 @@
-import { ucfirst, capitalize, camelCase, snake_case, leet, verlan, yoda } from "../exercise-1/string"
+import { ucfirst, capitalize, camelCase, snake_case, leet, verlan, yoda, vig } from "../exercise-1/string"
 
 declare global {
     interface String {
@@ -9,33 +9,38 @@ declare global {
         leet(): string;
         verlan(): string;
         yoda(): string;
+        vig(key: string): string;
     }
 }
 
-String.prototype.ucfirst = function() {
+String.prototype.ucfirst = function(): string {
     return ucfirst(this);
 };
 
-String.prototype.capitalize = function() {
+String.prototype.capitalize = function(): string {
     return capitalize(this);
 };
 
-String.prototype.camelCase = function() {
+String.prototype.camelCase = function(): string {
     return camelCase(this);
 };
 
-String.prototype.snake_case = function() {
+String.prototype.snake_case = function(): string {
     return snake_case(this);
 };
 
-String.prototype.leet = function() {
+String.prototype.leet = function(): string {
     return leet(this);
 };
 
-String.prototype.verlan = function() {
+String.prototype.verlan = function(): string {
     return verlan(this as string);
 };
 
-String.prototype.yoda = function() {
+String.prototype.yoda = function(): string {
     return yoda(this as string);
+};
+
+String.prototype.vig = function(key: string): string {
+    return vig(this, key);
 };
